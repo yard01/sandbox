3717a86375816cf4d538d348dac374447155696e
yard01 <yardmitry@gmail.com>
2017-04-07 15:57:56 +0300

    A info message was added into MainActivity.java [master]

--- androidexamples/src/main/java/easytest/catophot/ru/androidexamples/MainActivity.java
+++ androidexamples/src/main/java/easytest/catophot/ru/androidexamples/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("DEBUG_TAG", "debug message");
+        Log.i("INFO_TAG", "info message");
+
     }
 }
